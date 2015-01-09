bf06d05a1eeee5cad2ab0b43860a81b34a1f1a40
Michael Shim <imapp.help@gmail.com>
2015-01-08 22:56:49 -0800

    .

--- www/imad.js
+++ www/imad.js
@@ -3,7 +3,7 @@ cordova.define("com.ios.cordova-idfa.getIDFA", function(require, exports, module
     var exec = require('cordova/exec');
 
     var getIDFA = function (success, failure) {
-        exec(success, failure, "CDVIdfa", "advertisingIdentifier", []);
+        exec(success, failure, "CDVAdId", "get", []);
     };
 
     module.exports = getIDFA;
